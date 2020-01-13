b5d8651322a3b9e03cede9be5fe2c4555c3ccf3d
jj <jj@localhost>
2020-01-13 16:18:17 -0600

    first commit

---
+++ clinical-nlp-tools/src/main/java/com/melax/demo/clinical_nlp_tools/App.java
@@ -0,0 +1,13 @@
+package com.melax.demo.clinical_nlp_tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
