c8b586255d78d4a1ba09b0b288c2320fe93eaf78
xinyue <jj@localhost>
2020-01-14 11:26:37 -0600

    format

--- clinical-nlp-tools/src/main/java/com/melax/demo/format/InputRelationBIOFormat.java
+++ clinical-nlp-tools/src/main/java/com/melax/demo/format/InputRelationBIOFormat.java
@@ -164,7 +164,7 @@ public class InputRelationBIOFormat extends InputFormat {
   
   public static void main( String[] argv ) {
 	  
-	// Example of converting the relation BIO file to the clamp document
+	//Example of converting the relation BIO file to the clamp document
 	  File indir = new File( "input/" );
       File outdir = new File( "output/" );
       
